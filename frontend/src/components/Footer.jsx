f783c0b6bf88af5aa33ad4d92dc16e442aecf3d5
N02468 <mn3110403@gmail.com>
2025-01-17 17:35:31 +0500

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,54 @@
+// components/Footer.js
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+  color: #fff;
+  padding: 40px 50px;
+  margin-top: 50px; 
+  height: auto; 
+  width: 100%;
+  text-align: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    text-align: center;
+    height: auto;
+  }
+`;
+
+const CopyrightText = styled.div`
+  font-size: 14px;
+`;
+
+const AboutUs = styled.div`
+  font-size: 14px;
+  text-align: right;
+
+  @media (max-width: 768px) {
+    text-align: center;
+    margin-top: 20px;
+  }
+`;
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <CopyrightText>&copy; 2024 My Portfolio</CopyrightText>
+      <AboutUs>
+        <p><strong style={{"color":"white","font-family":"Comic-Neue,cursive","font-weight":"bolder"}}>About Us</strong></p>
+        <p style={{"color":"white","font-family":"Comic-Neue,cursive","font-weight":"bolder"}}>We create amazing web experiences for our users.</p>
+        <ion-icon name="logo-linkedin" style={{"font-size":"30px","justify-content":"space-evenly","margin-left":"1rem"}}></ion-icon>
+        <ion-icon name="logo-instagram"style={{"font-size":"30px","justify-content":"space-evenly","margin-left":"1rem"}}></ion-icon>
+        <ion-icon name="logo-facebook" style={{"font-size":"30px","justify-content":"space-evenly","margin-left":"1rem"}}></ion-icon>
+        <ion-icon name="logo-twitter"  style={{"font-size":"30px","justify-content":"space-evenly","margin-left":"1rem"}}></ion-icon>
+      </AboutUs>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
